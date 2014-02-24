a9267d0d3a85a397ccaa8c54d5d501d81fbee132
Tonko Balkonko <toni.cesarek@gmail.com>
2014-02-24 08:37:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CmsLight::Application.config.secret_key_base = '6f5176cf1d01ea72ec0178be79d759e49020ed19487fcbd3394062a272dca29d83a930549901357d45d5f138334f00ab4f6a5ac2a4a95a832e7e2684147db9f6'
