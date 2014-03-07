71802594965611b332e9c6bf383425be5fa88f32
Tonko Balkonko <toni.cesarek@gmail.com>
2014-03-07 12:40:35 +0000

    Triing to make article work part 1

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -12,4 +12,23 @@ class SubcategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @subcategory = @category.subcategories.build
   end
+  
+  def create
+    @category = Category.find(params[:id])
+    @subcategory = @category.subcategories.build(subcategory_params)
+ 
+    if @subcategory.save
+      redirect_to new_subcategory_path(id: @category.id)
+    else
+      render 'new'
+    end
+  end
+  
 end
+
+ 
+ private
+  def subcategory_params
+    params.require(:subcategory).permit(:name)
+  end
+
