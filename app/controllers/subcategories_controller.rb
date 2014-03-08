a8c5cda3ada366d16ac6c0b732f8578401e6afd5
tonko <toni.cesarek@gmail.com>
2014-03-08 08:13:47 +0100

    finelizes the subcategories#new and subcategories#destroy

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,46 +1,52 @@
 class SubcategoriesController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def edit
-     @subcategory = Subcategory.find(params[:id])
-  end
+	def edit
+		@subcategory = Subcategory.find(params[:id])
+	end
 
-  def update
-    
-    @subcategory = Subcategory.find(params[:id])
-   
-    if @subcategory.update(subcategory_params)
-      redirect_to action: 'new', id: @subcategory.category_id
-    else
-      render 'edit'
-    end
-  end
+	def update
 
-  def new
-    @category = Category.find(params[:id])
-    @subcategory = @category.subcategories.build
-  end
-  
-  def create
-    @category = Category.find(params[:id])
-    @subcategory = @category.subcategories.build(subcategory_params)
- 
-    if @subcategory.save
-      redirect_to new_subcategory_path(id: @category.id)
-    else
-      render 'new'
-    end
-  end
-  
+		@subcategory = Subcategory.find(params[:id])
+
+		if @subcategory.update(subcategory_params)
+			redirect_to action: 'new', id: @subcategory.category_id
+		else
+			render 'edit'
+		end
+	end
+
+	def new
+		@category = Category.find(params[:id])
+		@subcategory = @category.subcategories.build
+	end
+
+	def create
+		@category = Category.find(params[:id])
+		@subcategory = @category.subcategories.build(subcategory_params)
+
+		if @subcategory.save
+			redirect_to new_subcategory_path(id: @category.id)
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@subcategory = Subcategory.find(params[:id])
+		@subcategory.destroy
+
+		redirect_to action: 'new', id: @subcategory.category_id
+	end
 end
 
- 
- private
-  def subcategory_params
-    params.require(:subcategory).permit(:name, :body)
-  end
+
+private
+def subcategory_params
+	params.require(:subcategory).permit(:name, :body)
+end
 
