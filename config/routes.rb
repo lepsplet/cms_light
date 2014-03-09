ca9c920a8e2e1c459a2ffc6cc271930cd6bd8d06
tonko <toni.cesarek@gmail.com>
2014-03-09 09:21:36 +0100

    generates news controllers and views for public

--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,64 @@
 CmsLight::Application.routes.draw do
-  
-  root 'primecategories#new'
 
-  #get "articles/index"
-  #get "articles/show"
-  #get "articles/edit"
-  #get "articles/new"
-  #get "subcategories/index"
-  #get "subcategories/show"
-  #get "subcategories/edit"
-  #get "subcategories/new"
-  #get "categories/index"
-  #get "categories/show"
-  #get "categories/edit"
-  #get "categories/new"
-  
-  resources :primecategories, :categories, :subcategories, :articles
-  
-  
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	root 'news#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	resources :news, only: [ :index, :new ]
+	resources :primecategories, :categories, :subcategories, :articles
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	# The priority is based upon order of creation: first created -> highest priority.
+	# See how all your routes lay out with "rake routes".
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# You can have the root of your site routed with "root"
+	# root 'welcome#index'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+	# Example of regular route:
+	#   get 'products/:id' => 'catalog#view'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+	# Example of named route that can be invoked with purchase_url(id: product.id)
+	#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+	# Example resource route (maps HTTP verbs to controller actions automatically):
+	#   resources :products
+
+	# Example resource route with options:
+	#   resources :products do
+	#     member do
+	#       get 'short'
+	#       post 'toggle'
+	#     end
+	#
+	#     collection do
+	#       get 'sold'
+	#     end
+	#   end
+
+	# Example resource route with sub-resources:
+	#   resources :products do
+	#     resources :comments, :sales
+	#     resource :seller
+	#   end
+
+	# Example resource route with more complex sub-resources:
+	#   resources :products do
+	#     resources :comments
+	#     resources :sales do
+	#       get 'recent', on: :collection
+	#     end
+	#   end
+
+	# Example resource route with concerns:
+	#   concern :toggleable do
+	#     post 'toggle'
+	#   end
+	#   resources :posts, concerns: :toggleable
+	#   resources :photos, concerns: :toggleable
+
+	# Example resource route within a namespace:
+	#   namespace :admin do
+	#     # Directs /admin/products/* to Admin::ProductsController
+	#     # (app/controllers/admin/products_controller.rb)
+	#     resources :products
+	#   end
 end
